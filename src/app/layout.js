4749ed10dd06ab50e1a24933d835324d16ad1e90
Dota43ver <facundoagueroc@gmail.com>
2023-09-23 10:51:18 -0300

    responsive body

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Blog App',
-  description: 'The best blog app!',
+  description: 'El mejor blog de argentina!',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='container'>
+          <div className='wrapper'>
+        {children}
+          </div>
+        </div>
+        </body>
     </html>
   )
 }
