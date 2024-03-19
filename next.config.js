1953048ed393914a2c297fbb2a817f80452d5488
Dota43ver <facundoagueroc@gmail.com>
2024-03-19 20:54:32 -0300

    vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
-      }
+  experimental: {
+    appDir: false,
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
+  }
 }
 
 module.exports = nextConfig
