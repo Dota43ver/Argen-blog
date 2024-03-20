98e33b339aa2730a55956660fd94fa0330d740f0
Dota43ver <facundoagueroc@gmail.com>
2024-03-20 09:23:50 -0300

    vercel4

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,19 +1,22 @@
-"use client";
-
-import Image from "next/image";
-import styles from "./writePage.module.css";
-import { useEffect, useState } from "react";
-import "react-quill/dist/quill.bubble.css";
-import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
+"use client"
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
+import styles from './writePage.module.css';
+import { useEffect, useState } from 'react';
+import 'react-quill/dist/quill.bubble.css'; // Puedes mantener esto aquí si solo importa estilos CSS
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 import {
   getStorage,
   ref,
   uploadBytesResumable,
   getDownloadURL,
-} from "firebase/storage";
-import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
+} from 'firebase/storage';
+import { app } from '@/utils/firebase';
+
+// Importación dinámica de ReactQuill con SSR desactivado
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+
 
 const WritePage = () => {
   const { status } = useSession();
