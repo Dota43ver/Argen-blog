555c087a673a55bc67502b80c0077152f0d7d628
Dota43ver <facundoagueroc@gmail.com>
2024-03-21 10:48:52 -0300

    resuelvo bug de categorias

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -6,11 +6,11 @@ const MenuCategories = () => {
   return (
     <div className={styles.categoryList}>
         <Link href="/blog?cat=anime" className={`${styles.categoryItem} ${styles.anime}`}>Anime</Link>
-        <Link href="/blog" className={`${styles.categoryItem} ${styles.deportes}`}>Deportes</Link>
-        <Link href="/blog" className={`${styles.categoryItem} ${styles.comida}`}>Comida</Link>
-        <Link href="/blog" className={`${styles.categoryItem} ${styles.viajes}`}>Viajes</Link>
-        <Link href="/blog" className={`${styles.categoryItem} ${styles.general}`}>General</Link>
-        <Link href="/blog" className={`${styles.categoryItem} ${styles.coding}`}>Coding</Link>
+        <Link href="/blog?cat=deportes" className={`${styles.categoryItem} ${styles.deportes}`}>Deportes</Link>
+        <Link href="/blog?cat=comida" className={`${styles.categoryItem} ${styles.comida}`}>Comida</Link>
+        <Link href="/blog?cat=viajes" className={`${styles.categoryItem} ${styles.viajes}`}>Viajes</Link>
+        <Link href="/blog?cat=general" className={`${styles.categoryItem} ${styles.general}`}>General</Link>
+        <Link href="/blog?cat=coding" className={`${styles.categoryItem} ${styles.coding}`}>Coding</Link>
       </div>
   )
 }
